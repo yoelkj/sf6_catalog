3adcab53db0774967830b39795b135d19216baf5
¨Yoel¨ <yoel.velasquez.valencia@gmail.com>
2022-06-16 12:44:57 -0500

    Upgrade catalog templates

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -11,16 +11,30 @@ class CatalogController extends AbstractController
     #[Route("/")]
     public function index(): Response
     {
-        return new Response('Catalog homepage');
+
+        $rows = [
+            ['name' => 'Gangsta\'s Paradise', 'desc' => 'Coolio'],
+            ['name' => 'Waterfalls', 'desc' => 'TLC'],
+            ['name' => 'Creep', 'desc' => 'Radiohead'],
+            ['name' => 'Kiss from a Rose', 'desc' => 'Seal'],
+            ['name' => 'On Bended Knee', 'desc' => 'Boyz II Men'],
+            ['name' => 'Fantasy', 'desc' => 'Mariah Carey'],
+        ];
+
+        return $this->render('catalog/index.html.twig', [
+            'title' => 'Catalog home page',
+            'rows' => $rows
+        ]);
     }
 
     #[Route('/browse/{slug}')]
     public function browse(string $slug = null): Response
     {
-
         $title = ($slug) ? 'Category: '.ucwords(str_replace('-', ' ', $slug)): 'All Categories';
 
-        return new Response($title);
+        return $this->render('catalog/index.html.twig', [
+            'title' => $title
+        ]);
     }
-    
+
 }
