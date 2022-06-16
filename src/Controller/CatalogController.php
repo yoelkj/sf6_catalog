6ea2d2d1be4e7eae64cc5cb09fc210b631d9ca9d
¨Yoel¨ <yoel.velasquez.valencia@gmail.com>
2022-06-16 13:14:28 -0500

    Upgrade catalog controller  add api return json data

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CatalogController extends AbstractController
 {
-    #[Route("/")]
+    #[Route("/", name: 'app_homepage')]
     public function index(): Response
     {
 
@@ -27,14 +28,32 @@ class CatalogController extends AbstractController
         ]);
     }
 
-    #[Route('/browse/{slug}')]
+    #[Route('/browse/{slug}', name: 'app_browse')]
     public function browse(string $slug = null): Response
     {
         $title = ($slug) ? 'Category: '.ucwords(str_replace('-', ' ', $slug)): 'All Categories';
 
-        return $this->render('catalog/index.html.twig', [
+        return $this->render('catalog/browse.html.twig', [
             'title' => $title
         ]);
     }
+
+    #[Route('/api/products/{id<\d+>}', methods: ['GET'], name: 'app_ajax_produc_detail')]
+    public function getProduct(int $id, LoggerInterface $logger): Response
+    {
+        // TODO query the database
+        $data = [
+            'id' => $id,
+            'name' => 'product name',
+            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
+        ];
+
+        $logger->info('Returning API response for product {data}', [
+            'product' => $id,
+        ]);
+
+        return $this->json($data);
+    }
+
 
 }
