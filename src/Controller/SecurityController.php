20b69c397d7938249e21ed2ac004a3fecc1a47cf
¨Yoel¨ <yoel.velasquez.valencia@gmail.com>
2022-06-16 19:30:30 -0500

    Cutom login and error manager

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function login(AuthenticationUtils $auth_utils): Response
+    {
+        return $this->render('security/login.html.twig', [
+            'error' => $auth_utils->getLastAuthenticationError(),
+        ]);
+    }
+}
---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+
+class LoginFormAuthenticator extends AbstractAuthenticator
+{
+    private UserRepository $user_repo;
+    private RouterInterface $router;
+    
+    public function __construct(UserRepository $user_repo, RouterInterface $router)
+    {
+        $this->user_repo = $user_repo;
+        $this->router = $router;
+    }
+
+    public function supports(Request $request): ?bool
+    {
+        return $request->getPathInfo() === '/login' && $request->isMethod('POST');
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+
+        return new Passport(
+            new UserBadge($email, function($user_ident){
+                $user = $this->user_repo->findOneBy([
+                    'email' => $user_ident
+                ]);
+
+                if(!$user){
+                    throw new UserNotFoundException();
+                }
+
+                return $user;
+            }),
+            new CustomCredentials(function($credentials, User $user){
+                return $credentials === 'tada';
+            }, $password)
+        );
+
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        return new RedirectResponse(
+            $this->router->generate('app_homepage')
+        );
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
+        return new RedirectResponse(
+            $this->router->generate('app_login')
+        );
+    }
+
+//    public function start(Request $request, AuthenticationException $authException = null): Response
+//    {
+//        /*
+//         * If you would like this class to control what happens when an anonymous user accesses a
+//         * protected page (e.g. redirect to /login), uncomment this method and make this class
+//         * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface.
+//         *
+//         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
+//         */
+//    }
+}
